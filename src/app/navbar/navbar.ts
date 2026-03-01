c3c315d23779397b00e441af6e3be3c2540ca003
Benlaharche Adam <adam.benlaharche@epfedu.fr>
2026-03-01 13:08:18 +0100

    Ajout bouton connexion

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
-import { TitleCasePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [TitleCasePipe, RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
 @Input({ required: true }) title! : string
+
+isOpen = false;
+
+toggleMenu() {
+  this.isOpen = !this.isOpen;
+}
+
+closeMenu() {
+  this.isOpen = false;
+}
 }
