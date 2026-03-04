2b04c3a03be80714ee96e8732fc27d4a862e5025
“solene_martinez” <solene.martinez@epfedu.fr>
2026-03-04 03:17:47 +0100

    mini jeu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { AddMovie } from './add-movie/add-movie';
 import { UpdateMovie } from './update-movie/update-movie';
 import { MoviePage } from './movie-page/movie-page';
 import { Contact } from './contact/contact';
+import { MiniGame } from './mini-game/mini-game';
+
 
 export const routes: Routes = [
     { path: '', component: Home},
     { path: 'movies', component: MoviesList},
     { path: 'add-movie', component: AddMovie},
     { path: 'update-movie/:id', component: UpdateMovie},
-    { path: 'contact', component: Contact}
-    ,
+    { path: 'contact', component: Contact},
     { path: 'movies/:id', component: MoviePage },
+    { path: 'mini-game', component: MiniGame }
 ];
